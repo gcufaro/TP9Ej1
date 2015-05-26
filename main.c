c070e6e24168a7883751c526f8f44af1cd38a1e8
gcufaro <gcufaro@itba.edu.ar>
2015-05-26 18:32:29 -0300

    Cambio 2
    Se agregaron subrutinas clear y toggle

--- main.c
+++ main.c
@@ -8,7 +8,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void bitClr(int a);
 
 /*
  * 
@@ -33,6 +32,9 @@ typedef struct
 } PORT16;
 
 void bitSet(int a, MYPORT *p1);
+void bitClr(int b, MYPORT *p2);
+void bitToggle(int c, MYPORT *p3);
+
 
 int main(int argc, char** argv) {
 
@@ -80,5 +82,76 @@ void bitSet(int a, MYPORT *p1)
     }
 }
 
+void bitClr(int b, MYPORT *p1)
+{
+    switch (b)
+    {
+        case 0:    p1->bit0=0; 
+            break;
+        case 1:    p1->bit1=0;
+            break;
+        case 2:    p1->bit2=0; 
+            break;
+        case 3:    p1->bit3=0;
+            break;
+        case 4:    p1->bit4=0;
+            break;
+        case 5:    p1->bit5=0;
+            break;
+        case 6:    p1->bit6=0;
+            break;
+        case 7:    p1->bit7=0;
+            break;
+        default: printf("Bit no valido\n");
+            break;
+    }
+}
 
-
+void bitToggle(int c, MYPORT *p3)
+{
+    switch (c)
+    {
+        case 0:    if(p3->bit0==0)
+                    (p3->bit0=1); else{
+                    p3->bit0=0;
+                    }; 
+            break;
+        case 1:    if(p3->bit1==0)
+                    (p3->bit1=1); else{
+                    p3->bit1=0;
+                    }; 
+            break;
+        case 2:    if(p3->bit2==0)
+                    (p3->bit2=1); else{
+                    p3->bit2=0;
+                    }; 
+            break;
+        case 3:    if(p3->bit3==0)
+                    (p3->bit3=1); else{
+                    p3->bit3=0;
+                    }; 
+            break;
+        case 4:    if(p3->bit4==0)
+                    (p3->bit4=1); else{
+                    p3->bit4=0;
+                    }; 
+            break;
+        case 5:    if(p3->bit5==0)
+                    (p3->bit5=1); else{
+                    p3->bit5=0;
+                    }; 
+            break;
+        case 6:    if(p3->bit6==0)
+                    (p3->bit6=1); else{
+                    p3->bit6=0;
+                    }; 
+            break;
+        case 7:    if(p3->bit7==0)
+                    (p3->bit7=1); else{
+                    p3->bit7=0;
+                    }; 
+            break;
+        default: printf("Bit no valido\n");
+            break;
+    }
+}
