d84ddf06a5035cc582ca6f703b9c2a08e980cee4
Mahler1459 <agustinderuschi@hotmail.com>
2015-05-30 13:48:09 +0000

    FSM con problemas 2

--- ppal.c
+++ ppal.c
@@ -1,22 +1,36 @@
 #include <stdio.h>
-
 #include "FSM.h"	/*FSM engine*/
 #include "FSMTABLE.h"   /*FSM Table*/
 #include "termlib.h"    /*Enable the use of getch and getche see termlib.c*/
 
 
+
+
+
 STATE *p2state=NULL;	/*Used to store FSM state*/
-
-
 
 #define FIN_ARCHIVO  'Q'
 
-
-
-int main (void)
+int main (int argc, char** argv)
 {
+MYPORT portA;
+MYPORT portB;
 
+MYPORT *pA;
+MYPORT *pB;
+pA=&portA;
+pB=&portB;
+
+PORT16 portD;
+
+portD.LS = portB;
+portD.MS = portA;
+
+
+//maquina de estado
      	int evento;
+        
+        
 
 	p2state=FSM_GetInitState();// Inicializo la FSM con el estado inicial
 
@@ -24,8 +38,7 @@ int main (void)
 	
 	p2state=fsm(p2state,evento);      //Se lo paso a la maquina de estados
 
-	printf("Total de coincidencias");
-  	getchar();
+	getchar();
 
     
 }
